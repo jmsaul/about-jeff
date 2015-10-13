94fb964eb3289041ed77b0ee99ae77d19e589296
Jeff Saul <scaleup13@gmail.com>
2015-10-13 11:34:18 -0600

    Added new draft of Jeff webpage => about-jeff

--- index.php
+++ index.php
@@ -5,10 +5,14 @@
 		<title>Dr. Jeff Saul </title>
 	</head>
 	<body>
-		<h1>STEM Education Consultant</h1>
-		<h2>Contact Information</h2>
-		<p>Email:  scaleup13@gmail.com</p>
-		<p>Phone:  954-665-5373</p>
-
+		<h1>STEM Education Consultant / Albuquerque Bike Rider</h1>
+		<h2>Work Email</h2>
+			<p>Email:  scaleup13 at gmail.com</p>
+		<h2>Personal Email</h2>
+			<p>Email:  jeffsaul at me dot com</p>
+			<p>Email:  abqbikerider at gmail dot com</p>
+		<hr>
+		<p>I have been teaching introductory physics and astronomy as a tutor and teacher for over 30 years.  I earned a Ph.D in Physics with a dissertation in physics education research at the Universitiy of Maryland, College Park.  Ny teaching and research have focued on activitypbased learning</p>
+		<P>I am married to Joy, who is retired and mainly works as an extra and does part-time marketing.  I enjoy</P>
 	</body>
 </html>
