d8d341d087de614792119f011c8bfb6da4593cb9
Jeff Saul <scaleup13@gmail.com>
2015-10-13 11:47:58 -0600

    Added new draft of Jeff webpage => about-jeff

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
-<!DOCTYPE> html>
+<!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<title>Dr. Jeff Saul </title>
+		<title>Jeff Saul</title>
 	</head>
 	<body>
-		<h1>STEM Education Consultant / Albuquerque Bike Rider</h1>
+		<h1>Jeff Saul <br>STEM Education Consultant / Albuquerque Bike Rider</h1>
 		<h2>Work Email</h2>
-			<p>Email:  scaleup13 at gmail.com</p>
+		<p>Email:  scaleup13 at gmail.com</p>
 		<h2>Personal Email</h2>
-			<p>Email:  jeffsaul at me dot com</p>
-			<p>Email:  abqbikerider at gmail dot com</p>
+		<p>Email:  jeffsaul at me dot com</p>
+		<p>Email:  abqbikerider at gmail dot com</p>
 		<hr>
 		<p>I have been teaching introductory physics and astronomy as a tutor and teacher for over 30 years.  I earned a Ph.D in Physics with a dissertation in physics education research at the Universitiy of Maryland, College Park.  Ny teaching and research have focued on activity-based learning. </p>
-		<P>I am married to Joy, who is retired and mainly works as an extra and does part-time marketing.  I enjoy</P>
+		<p>I am married to Joy, who is retired and mainly works as an extra and does part-time marketing.  I enjoy</p>
 	</body>
 </html>
