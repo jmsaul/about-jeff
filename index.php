19d4c66411ea807fa362e5a562c6444ded3b5b1b
Jeff Saul <scaleup13@gmail.com>
2015-10-13 11:15:38 -0600

    Added new draft of Jeff webpage => about-jeff

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE> html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Dr. Jeff Saul </title>
+	</head>
+	<body>
+		<h1>STEM Education Consultant</h1>
+		<h2>Contact Information</h2>
+		<p>Email:  scaleup13@gmail.com</p>
+		<p>Phone:  954-665-5373</p>
+
+	</body>
+</html>
