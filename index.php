9a5f14e23cee0163309943645947803cbf023797
Jeff Saul <scaleup13@gmail.com>
2015-10-13 11:53:44 -0600

    Added new draft of Jeff webpage => about-jeff

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 		<p>Email:  jeffsaul at me dot com</p>
 		<p>Email:  abqbikerider at gmail dot com</p>
 		<hr>
-		<p>I have been teaching introductory physics and astronomy as a tutor and teacher for over 30 years.  I earned a Ph.D in Physics with a dissertation in physics education research at the Universitiy of Maryland, College Park.  Ny teaching and research have focued on activity-based learning. </p>
+		<p>I have been teaching introductory physics and astronomy as a tutor and teacher for over 30 years.  I earned a Ph.D in Physics with a dissertation in physics education research at the Universitiy of Maryland, College Park.  Ny teaching and research have focused on activity-based learning,learning by doing. This results in combined lecture/lab classes where most of class time is used having the students do small group activities and report back to the class on their results with a class Q and A session.  The main projects I have worked on are SCALE-UP and Modeling Instruction as well as developing assessments like the Maryland Phyiscs Excectaion Survey, which looks at students' cognitive beliefs about physics and how they learn physics.  My most recent job was being a Physics and Astronomy lecturer for the past six years.</p>
 		<p>I am married to Joy, who is retired and mainly works as an extra and does part-time marketing.  I enjoy</p>
 	</body>
 </html>
